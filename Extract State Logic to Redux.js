0255ec08874a21e7d9d71c88e771bf2b1f2f68d6
Raghav Singal <32788339+raghav54@users.noreply.github.com>
2021-10-23 16:05:02 +0530

    Create Extract State Logic to Redux.js
    Extract State Logic to Redux
    Now that you finished the React component, you need to move the logic it's performing locally in its state into Redux. This is the first step to connect the simple React app to Redux. The only functionality your app has is to add new messages from the user to an unordered list. The example is simple in order to demonstrate how React and Redux work together.
    
    First, define an action type ADD and set it to a const ADD. Next, define an action creator addMessage() which creates the action to add a message. You'll need to pass a message to this action creator and include the message in the returned action.
    
    Then create a reducer called messageReducer() that handles the state for the messages. The initial state should equal an empty array. This reducer should add a message to the array of messages held in state, or return the current state. Finally, create your Redux store and pass it the reducer.

---
+++ Extract State Logic to Redux.js
@@ -0,0 +1,15 @@
+// Define ADD, addMessage(), messageReducer(), and store here:
+const ADD = 'ADD';
+const addMessage = (message) => {
+return {
+  type: ADD,
+  message
+};
+}
+const messageReducer = (state=[],action) => {
+switch(action.type){
+  case ADD: return state.concat([action.message]);
+  default: return state;
+}
+}
+Redux.createStore(messageReducer)
